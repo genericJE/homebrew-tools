fa0bfc91c0b154723c328f7687cd232e2e644278
John Eric <johnericvonrosen@gmail.com>
2026-04-05 10:03:27 +0200

    coffeinate: update to v0.2.0

--- Formula/coffeinate.rb
+++ Formula/coffeinate.rb
@@ -1,8 +1,8 @@
 class Coffeinate < Formula
   desc "Keep your Mac awake, with style"
   homepage "https://github.com/genericJE/coffeinate"
-  url "https://github.com/genericJE/coffeinate/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "a917ce6af3d0df7c7147e7d21020245c201823c097749668d313af6f267105a4"
+  url "https://github.com/genericJE/coffeinate/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "500317e470654c00485f45df9bde34b21d116d152194d490228b423eb705e69a"
   license "MIT"
 
   depends_on :macos
