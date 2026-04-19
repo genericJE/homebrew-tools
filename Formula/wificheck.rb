0d75b47e1a486dce434f68245df0a7a4ec8228c4
John Eric <johnericvonrosen@gmail.com>
2026-04-19 20:02:36 +0200

    wificheck 0.1.2

--- Formula/wificheck.rb
+++ Formula/wificheck.rb
@@ -1,8 +1,8 @@
 class Wificheck < Formula
   desc "Notify when an unstable Wi-Fi connection comes back online"
   homepage "https://github.com/genericJE/wificheck"
-  url "https://github.com/genericJE/wificheck/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "060d58f9d30ef628275fb22d572af24c30fbe58cbbe5e097de6567bee967c9e7"
+  url "https://github.com/genericJE/wificheck/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "6575be9c972f26d758d3b4c22d0685ba55cf9724f338fb8c2a13e588d287bcdc"
   license "MIT"
 
   depends_on :macos
