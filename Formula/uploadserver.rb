1c7c86eb88ee457d8bdebc79b0a796018a287ac6
John Eric <johnericvonrosen@gmail.com>
2026-06-05 15:41:35 +0200

    uploadserver 1.1.0

--- Formula/uploadserver.rb
+++ Formula/uploadserver.rb
@@ -1,8 +1,8 @@
 class Uploadserver < Formula
   desc "Minimal upload-only HTTP server with drag-and-drop web UI"
   homepage "https://github.com/genericJE/uploadserver"
-  url "https://github.com/genericJE/uploadserver/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "f6cb60d7b314e7504ab18788718a8563d7eb9269832f01e9714eccd773885389"
+  url "https://github.com/genericJE/uploadserver/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "68152900a79e7c5bbf40b366c943000d3bd81ec4eaed6e402d7110a54d748cc9"
   license "MIT"
 
   depends_on "python@3"
