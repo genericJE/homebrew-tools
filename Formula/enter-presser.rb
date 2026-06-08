ff571ac5ef2c921115a07068e7876fa29de4db58
John Eric von Rosen <johneric.vonrosen@bugshell.com>
2026-06-08 12:41:42 +0200

    Update enter-presser formula to v0.1.2

--- Formula/enter-presser.rb
+++ Formula/enter-presser.rb
@@ -3,21 +3,11 @@ class EnterPresser < Formula
 
   desc "Wait for a timer, then press Enter in the focused app"
   homepage "https://github.com/genericJE/enter-presser"
-  url "https://github.com/genericJE/enter-presser/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "3ef8e224441767115f03b057ddb6845ff52e6e11947468cb5e3ec843d816a125"
+  url "https://github.com/genericJE/enter-presser/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   depends_on "python@3.13"
-
-  resource "pynput" do
-    url "https://files.pythonhosted.org/packages/source/p/pynput/pynput-1.8.1.tar.gz"
-    sha256 "fc53c26f5e042d963b4e349266dc38305e1db1b6de9abc13f539808f82e00d78"
-  end
-
-  resource "six" do
-    url "https://files.pythonhosted.org/packages/source/s/six/six-1.17.0.tar.gz"
-    sha256 "ff70335d468e7eb6ec65b95b99d3a2836546063f63acc5171de367e834932a81"
-  end
 
   def install
     virtualenv_install_with_resources
@@ -28,3 +18,4 @@ class EnterPresser < Formula
     assert_match "Dry run", shell_output("#{bin}/enter-presser -t 0 --dry-run")
   end
 end
+
